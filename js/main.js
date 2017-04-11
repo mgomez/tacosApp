b35756c460c0892dbedef10b8798fc0aef32ff28
Cesar Gomez <00.13.zkr@gmail.com>
2017-04-11 15:51:25 -0500

    imagen paps

--- js/main.js
+++ js/main.js
@@ -10,17 +10,17 @@ $(function() {
         });
     });
 
-    $(document).on("click", ".avatar", function() {
-        var id = $(this).data("id");
-        var usuario = $.Enumerable.From(Usuarios).Where(function(el) {
-            return el.Value.id === id;
-        }).FirstOrDefault();
-        console.log(usuario);
-        $.get("views/Usuario.html", function(template) {
-            $("#tacoContainer").handlebars(template, usuario.Value);
-        });
-        navigator.vibrate(100);
-    });
+    //$(document).on("click", ".avatar", function() {
+    //var id = $(this).data("id");
+    //var usuario = $.Enumerable.From(Usuarios).Where(function(el) {
+    //return el.Value.id === id;
+    //}).FirstOrDefault();
+    //console.log(usuario);
+    //$.get("views/Usuario.html", function(template) {
+    //$("#tacoContainer").handlebars(template, usuario.Value);
+    //});
+    //navigator.vibrate(100);
+    //});
 });
 
 function nuevoUsuario(userId, nombre, foto, fecha, listo) {
@@ -44,6 +44,7 @@ function onDeviceReady() {
         destinationType: Camera.DestinationType.DATA_URL,
         targetWidth: (device.platform !== 'Android') ? 150 : 750
     };
+
     $("#NewImg").on("click", function() {
         getPicture({
             config: config,
@@ -52,6 +53,24 @@ function onDeviceReady() {
             },
             onFail: function(message) {
                 alert(message);
+            }
+        });
+    });
+
+    $(document).on("click", ".avatar", function() {
+        var $avatar = $(this);
+        var idfb = $avatar.data("idfb");
+        getPicture({
+            config: config,
+            onSuccess: function(imageData) {
+                var img = "data:image/jpeg;base64," + imageData;
+                $avatar.find(".avatar-img").attr("src", img);
+                firebase.database().ref('usuarios/' + idfb).set({
+                    "foto": img
+                });
+            },
+            onFail: function(message) {
+                alert("ERROR: " + message);
             }
         });
     });
