34d75a6569b076de03ac78d78e899a57572f7fdf
Cesar Gomez <00.13.zkr@gmail.com>
2017-04-11 12:08:30 -0500

    firebase

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+$(function() {
+    var tacosDB = firebase.database().ref('/usuarios');
+
+    tacosDB.on('value', function(data) {
+        console.log("value", data.key, data.val());
+        var usuarios = data.val();
+        $.get("views/_tbTacos.html", function(template) {
+            $("#rolTable-content").handlebars(template, usuarios);
+        });
+    });
+
+    tacosDB.on("child_changed", function(data) {
+        console.log("child_changed", data.key, data.val());
+    });
+});
+
+function nuevoUsuario(userId, nombre, foto, fecha, listo) {
+    //default
+    var newPostKey = firebase.database().ref().child('usuarios').push().key;
+    return firebase.database().ref('usuarios/' + newPostKey).set({
+        "nombre": "Gerson Clemente",
+        "foto": "https://lh3.googleusercontent.com/-UI8vYio8jD4/AAAAAAAAAAI/AAAAAAAAAAA/kPF4DtNg6pk/s64-c/101746310403354218665.jpg",
+        "fecha": "01/04/1013",
+        "listo": false
+    });
+}
+
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
